1c8f42b80ad6d987bf9e2f10b218c3080149b38e
Dinush2k <dinushkaran2021@gmail.com>
2023-01-30 11:26:33 +0530

    Feature: Mobile menu

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+const navToggler = document.querySelector(".nav-toggler");
+
+navToggler.addEventListener("click", () => {
+  const headerMenu = document.querySelector("ul.header--menu");
+
+  const navIcon = document.querySelectorAll(".navIcon");
+  headerMenu.classList.toggle("show");
+
+  navIcon.forEach((icon) => {
+    icon.classList.toggle("hidden");
+  });
+});
