861d43cb0b33169dde2f56f53525e442943c2e29
Dinush2k <dinushkaran2021@gmail.com>
2023-01-30 13:13:35 +0530

    Feature: Mobile menu fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,19 @@
 const navToggler = document.querySelector(".nav-toggler");
+const navLinks = document.querySelectorAll(".header--menu>li>a");
 
-navToggler.addEventListener("click", () => {
+navToggler.addEventListener("click", changeMenuState);
+
+function changeMenuState() {
   const headerMenu = document.querySelector("ul.header--menu");
-
   const navIcon = document.querySelectorAll(".navIcon");
+
+  // Show menu programatically
   headerMenu.classList.toggle("show");
 
   navIcon.forEach((icon) => {
     icon.classList.toggle("hidden");
   });
-});
+}
 
 function navbarFixed() {
   const header_dom = document.querySelector(".header");
@@ -28,7 +32,6 @@ function navbarFixed() {
 
 function setMenuActive() {
   const sections = document.querySelectorAll("section");
-  const navLinks = document.querySelectorAll(".header--menu__item>a");
 
   window.addEventListener("scroll", () => {
     let current = "";
@@ -51,5 +54,13 @@ function setMenuActive() {
   });
 }
 
+function onMenuClick() {
+  console.log(navLinks);
+  for (let i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener("click", changeMenuState);
+  }
+}
+
 navbarFixed();
 setMenuActive();
+onMenuClick();
